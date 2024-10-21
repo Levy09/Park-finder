815c751928c12c5595d1b60acf918a92cdf839e6
Levy <levyaragao17@gmail.com>
2024-10-21 19:29:18 -0300

    adicionado estilos no card de vagas

--- src/app/components/CardStatus/CardStatus.jsx
+++ src/app/components/CardStatus/CardStatus.jsx
@@ -1,20 +1,27 @@
 
+import { CarSimple, LetterCircleP } from "@phosphor-icons/react/dist/ssr"
 
 
 
 export default function CardStatus() {
     return (
-        <div className="h-36 w-56 bg-blue-950 rounded-2xl flex flex-col justify-between text-white">
-            <div className="flex min-w-full">
-                <div className="flex-1 flex justify-center">
-                  1
+        <div className="p-4  gap-5 h-36 w-56 bg-blue-950 rounded-2xl flex flex-col justify-between   text-white ">
+            <div className="  flex min-w-full items-center">
+                <div className="flex-1 flex justify-start font-bold ">
+                  N° 001
                 </div>
-                <div className="flex-1 flex justify-center">
-                    2
-                </div>
+                <div className="flex1 ">
+    <div className="h-4 w-4 bg-green-500 rounded-full"></div>
+    
+</div>
+             
             </div>
-              <div className="flex justify-center">
-                footer
+            <div className=" gap-2 flex-1 flex justify-center items-center">
+               <CarSimple size={32} />
+               <p className="font-black text-xl">SVD2010</p>
+                </div>
+              <div className=" gap-2 flex justify-center">
+                E: &nbsp; <strong>00:00:00</strong>   S: &nbsp; <strong>00:00:00</strong>
               </div>
         </div>
 
